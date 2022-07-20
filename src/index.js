f969dd36d472cc0c2e4b0d78cd8ea4d827caa8c6
Juan Manuel Alvarez <jualvarez@fi.uba.ar>
2022-07-20 17:46:21 -0300

    emprolijando js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,9 @@
 
 
 $(document).ready(function () {
-  App = {
-    accJuego : '0xa09B6E0bE6E065d6dC5DA9a7d04340BE341bFA18',
-    addCreado : "0x9b6F4b9F0cDEBde2062Ad901Bb1a7cf08f88E54B",
-    web3Provider: null,
-    contracts: {},
-    accounts: {},
-
-    initWeb3: async function() {
-      if (window.ethereum) {
-        App.web3Provider = window.ethereum;
-        try {
-          await window.ethereum.enable();
-        } catch (error) {
-          console.error("User denied account access")
-        }
-      }
-      else if (window.web3) {
-        App.web3Provider = window.web3.currentProvider;
-      }
-      else {
-        App.web3Provider = new Web3.providers
-            .HttpProvider('https://rinkeby.infura.io/v3/4607a49ac0fd47dcb74f6282b60d18ff');
-      }
-      web3 = new Web3(App.web3Provider);
-      accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-
-    },
-
-    initContract: function() {
-      $.getJSON("Juego.json", function(data) {
-        App.contracts.Juego = TruffleContract(data);
-        App.contracts.Juego.setProvider(App.web3Provider);
-        return App.init();
-      });
-    },
-
-    init: async function() {
-      App.contracts.Juego.deployed().then(function(instance) {
-        hasbuInstance = instance;
-        //console.log(instance)
-        return hasbuInstance;
-      }).then(function(result) { 
-        const pong = result.ping().then((res) => {console.log(res)});
-        //const palabra2 = result.crearJugador({from: accounts[0]}).then((id) => console.log(id));
-      });
-    },
-
+  let fallos, aciertos, palabra_secreta, letras_probadas, letras_fallidas;
+  Front = {
+    
     _cadenaPermitida:  function(cadena) {
       let expresion = '';
   
@@ -138,14 +94,14 @@ $(document).ready(function () {
         for (let i = 0; i < palabra_secreta.length; i++) {
           if (palabra_secreta.charAt(i) == mascaraW[i]) {
             aciertos++;
-            App._escribirSpan(i, mascaraW[i]);
+            Front._escribirSpan(i, mascaraW[i]);
             letras_probadas += mascaraW[i];
           }
       }
       }
     }
       if (aciertos == palabra_secreta.replace(new RegExp(' ', 'g'), '').length) {
-        App._gane();
+        Front._gane();
       }
     },
 
@@ -171,7 +127,7 @@ $(document).ready(function () {
       div_letras_fallidas.html(html);
   
       if (fallos == 6) {
-        App._perdida();
+        Front._perdida();
       }
     },
 
@@ -184,7 +140,7 @@ $(document).ready(function () {
   
       $('#imagen_ahorcado').attr('src', '/img/ganador.png');
   
-      App._mostrarPalabra('gane');
+      Front._mostrarPalabra('gane');
     },
 
     _perdida: function() {
@@ -195,7 +151,158 @@ $(document).ready(function () {
       $('#boton_adivinar').attr('disabled', true);
   
       $('#imagen_ahorcado').attr('src', '/img/6error.png');
-      App._mostrarPalabra('perdida');
+      Front._mostrarPalabra('perdida');
+    },
+
+    _inicializar : function(word) {
+      fallos = 0;
+      aciertos = 0;
+      palabra_secreta = word;
+      letras_probadas = '';
+      letras_fallidas = '';
+      $('#imagen_ahorcado').attr('src', '/img/ahorcado.png');
+      $('#palabra').html('');
+      $('#letras_fallidas').html('');
+  
+      $('#probar_letra').val('');
+      $('#adivinar').val('');
+  
+      $('#boton_iniciar').attr("disabled", false);
+  
+      $('#probar_letra').attr("disabled", true);
+      $('#boton_probar').attr("disabled", true);
+  
+      $('#adivinar').attr("disabled", true);
+      $('#boton_adivinar').attr("disabled", true);
+  
+      $('#boton_iniciar').focus();
+    },
+
+    _finalizar: function() {
+      Front._inicializar();
+    },
+
+    _iniciar : function(word) {
+
+      if (word.length > 0) {
+        if (Front._cadenaPermitida(word)) {
+          word = word.toLowerCase();
+  
+          $('#boton_iniciar').attr("disabled", true);
+  
+          $('#probar_letra').attr("disabled", false);
+          $('#boton_probar').attr("disabled", false);
+  
+          $('#adivinar').attr("disabled", false);
+          $('#boton_adivinar').attr("disabled", false);
+  
+          Front._establecerEspacios();
+  
+          $('#probar_letra').focus();
+        } else {
+          $('#etiqueta_mensaje').html('Datos Incorrectos');
+          $('#cuerpo_mensaje').html('La palabra debe contener caracteres de la A a la Z únicamente.');
+          $('#mensaje').modal('show')
+  
+          $('#mensaje').on('hidden.bs.modal', function () {
+            $('#boton_iniciar').focus();
+          })
+        }
+      } else {
+        $('#etiqueta_mensaje').html('Datos Incorrectos');
+        $('#cuerpo_mensaje').html('Debe escribir la palabra secreta.');
+        $('#mensaje').modal('show')
+  
+        $('#mensaje').on('hidden.bs.modal', function () {
+          $('#boton_iniciar').focus();
+        })
+      }
+    },
+
+
+  }
+
+
+  App = {
+    accJuego : '0xa09B6E0bE6E065d6dC5DA9a7d04340BE341bFA18',
+    addCreado : "0x9b6F4b9F0cDEBde2062Ad901Bb1a7cf08f88E54B",
+    web3Provider: null,
+    contracts: {},
+    accounts: {},
+
+    initWeb3: async function() {
+      if (window.ethereum) {
+        App.web3Provider = window.ethereum;
+        try {
+          await window.ethereum.enable();
+        } catch (error) {
+          console.error("User denied account access")
+        }
+      }
+      else if (window.web3) {
+        App.web3Provider = window.web3.currentProvider;
+      }
+      else {
+        App.web3Provider = new Web3.providers
+            .HttpProvider('https://rinkeby.infura.io/v3/4607a49ac0fd47dcb74f6282b60d18ff');
+      }
+      web3 = new Web3(App.web3Provider);
+      accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+    },
+
+    initContract: function() {
+      $.getJSON("Juego.json", function(data) {
+        App.contracts.Juego = TruffleContract(data);
+        App.contracts.Juego.setProvider(App.web3Provider);
+        return App.init();
+      });
+    },
+
+    init: async function() {
+      App.contracts.Juego.deployed().then(function(instance) {
+        hasbuInstance = instance;
+        //console.log(instance)
+        return hasbuInstance;
+      }).then(function(result) { 
+        const pong = result.ping().then((res) => {console.log(res)});
+        //const palabra2 = result.crearJugador({from: accounts[0]}).then((id) => console.log(id));
+      });
+    },
+
+    instanciaJuego: function(){
+      return new Promise((resolve, reject) => {
+        $.getJSON("Juego.json").success(function(data) {
+          const web3 = new Web3(App.web3Provider);
+          const acc = web3.eth.accounts;
+
+          const juego = TruffleContract(data);
+          juego.setProvider(App.web3Provider);
+          var instance = juego.at(App.accJuego);
+
+          resolve([acc, instance, web3]);
+        })
+
+      })
+
+
+    } ,  
+
+    instanciaPalabra: function(add){
+      return new Promise((resolve, reject) => {
+        $.getJSON("Word.json", function(data){
+          const web3 = new Web3(App.web3Provider);  
+          const acc = web3.eth.accounts; 
+          
+          const word = TruffleContract(data);
+          word.setProvider(App.web3Provider);
+          var instance = word.at(add);
+
+          resolve( [acc, instance, web3] );
+
+      });
+    })
+
     },
 
     _probarLetra: function(addWord) {
@@ -208,29 +315,25 @@ $(document).ready(function () {
       const add = addWord.view.add
       if (letra != ' ') {
         if (letra.length > 0) {
-          if (App._cadenaPermitida(letra)) {
-            if (!App._verificarLetraProbada(letra)) {
+          if (Front._cadenaPermitida(letra)) {
+            if (!Front._verificarLetraProbada(letra)) {
 
-              $.getJSON("Word.json", function(data){
-                const web3 = new Web3(App.web3Provider);  // metamask
-                const acc = web3.eth.accounts; // array de accounts de metamask
-                
-                const word = TruffleContract(data);
-                word.setProvider(App.web3Provider);
-                var instance = word.at(add);
-                
-                instance.index(letra, {from: acc[0]}).then((res) => {
-                  const mask = res.logs[0].args.mask
-                  App._incluirLetra(mask);
-                  let aux = 0;
-                  for(let i=0; i<mask.length; i++){
-                    if(mask[i] != "-"){
-                      aux++;
-                    } 
-                  }
-                  if(aux == 0){
-                    App._incluirFallo(letra);
-                  }
+                App.instanciaPalabra(add).then((res, err) => {
+
+                  acc = res[0];
+                  instance = res[1];              
+                  instance.index(letra, {from: acc[0]}).then((res) => {
+                    const mask = res.logs[0].args.mask
+                    Front._incluirLetra(mask);
+                    let aux = 0;
+                    for(let i=0; i<mask.length; i++){
+                      if(mask[i] != "-"){
+                        aux++;
+                      } 
+                    }
+                    if(aux == 0){
+                      Front._incluirFallo(letra);
+                    }
                 });
               })
               
@@ -278,27 +381,24 @@ $(document).ready(function () {
       let palabra_adivinar = $('#adivinar').val()
   
       if (input_adivinar.val().length > 0) {
-        if (App._cadenaPermitida(input_adivinar.val())) {
+        if (Front._cadenaPermitida(input_adivinar.val())) {
 
-          $.getJSON("Word.json", function(data){
-            const web3 = new Web3(App.web3Provider);  // metamask
-            const acc = web3.eth.accounts; // array de accounts de metamask
-            
-            const word = TruffleContract(data);
-            word.setProvider(App.web3Provider);
-            var instance = word.at(add);
+            App.instanciaPalabra(add).then((res, err) => {
 
-            instance.chequearPalabra(palabra_adivinar, {from : acc[0]}).then((res, err) => {
+              acc = res[0];
+              instance = res[1];
+              instance.chequearPalabra(palabra_adivinar, {from : acc[0]}).then((res, err) => {
               if(res){
-                App._gane();
+                Front._gane();
                 App.minarNFT(palabra_adivinar);
               }
               else{
-                App._perdida();
+                Front._perdida();
               }
             })
 
           })
+
         } else {
           $('#etiqueta_mensaje').html('Datos Incorrectos');
           $('#cuerpo_mensaje').html('Sólo se permiten caracteres de la A a la Z únicamente.');
@@ -320,168 +420,79 @@ $(document).ready(function () {
       }
     },
 
-    _finalizar: function() {
-      App._inicializar();
-    },
-
     crearJugador: function(){
-      $.getJSON("Juego.json", function(data){
+
+        App.instanciaJuego().then((res, err) => {
+
+          acc = res[0];
+          instance = res[1];
+          instance.crearJugador({from: acc[0]})
+          document.getElementById('data').innerHTML = '<br> Cuenta: ' + acc[0];
         
-        const web3 = new Web3(App.web3Provider);
-        const acc = web3.eth.accounts;
-    
-        const juego = TruffleContract(data);
-        juego.setProvider(App.web3Provider);
-
-        const instance = juego.at(App.accJuego);
-
-        instance.crearJugador({from: acc[0]})
-
-        document.getElementById('data').innerHTML += '<br> Cuenta: ' + acc[0];
-        
-      
       })
+      
         
       },
-    
+
     elegirPalabra: function(){
 
       return new Promise((resolve, reject) => {
-        $.getJSON("Juego.json").success(function(data) {
+        
+        App.instanciaJuego().then((res, err) => {
 
-        const web3 = new Web3(App.web3Provider);
-        const acc = web3.eth.accounts;
-        //console.log(web3)
+          acc = res[0];
+          instance = res[1];
+          web3 = res[2];
 
-        const juego = TruffleContract(data);
-        juego.setProvider(App.web3Provider);
-        var instance = juego.at(App.accJuego);
-        instance.elegirPalabra({from: acc[0]}).then((address, errAdd) =>{
-          if(!errAdd){
-            add = address.logs[0].args.add
-            App.mostrarBlockPalabra(add).then((resWord, errWord) => {
-              if(!errWord){
-                App._inicializar(resWord);
-                App._iniciar(resWord);
-                resolve(add);
-              }
+        web3.eth.sendTransaction(
+          { from: acc[0], to: App.addCreado, value: (0.00006) *1e18}, 
+            (err,res) =>{
+              if(res){ 
+                instance.elegirPalabra({from: acc[0]}).then((address, errAdd) =>{
+                if(!errAdd){
+                  add = address.logs[0].args.add
+                  App.mostrarBlockPalabra(add).then((resWord, errWord) => {
+                    if(!errWord){
+                      Front._inicializar(resWord);
+                      Front._iniciar(resWord);
+                      resolve(add);
+                    }
 
             });
           }
-        })
-        
-        web3.eth.sendTransaction({
-          from: acc[0],
-          to: App.addCreado,
-          value: (0.00006) *1e18, // Lo que es un equivalente a 0.1USD
-        }, (res,err) =>{
-        });
-        
-
-
+        })}
       });
+        
+    })
     })
   
     },
 
     mostrarBlockPalabra: function(add){
-        return new Promise((resolve, reject) => {
-          $.getJSON("Word.json", function(data){
-          const web3 = new Web3(App.web3Provider);  
-          const acc = web3.eth.accounts; 
+      return new Promise((resolve, reject) => {
+        App.instanciaPalabra(add).then((res, err) => {
           
-          const word = TruffleContract(data);
-          word.setProvider(App.web3Provider);
-          var instance = word.at(add);
-          instance.getStr.call({from: acc[0]}).then((res) => {
-            resolve(res);
-          });
-
-
-      })
-    });
+          acc = res[0];
+          instance = res[1];
+  
+        instance.getStr.call({from: acc[0]}).then((res) => {
+          resolve(res);
+        });
+      });
+  });
   },
 
-    _inicializar : function(word) {
-      fallos = 0;
-      aciertos = 0;
-      palabra_secreta = word;
-      //console.log(palabra_secreta);
-      letras_probadas = '';
-      letras_fallidas = '';
-      $('#imagen_ahorcado').attr('src', '/img/ahorcado.png');
-      $('#palabra').html('');
-      $('#letras_fallidas').html('');
-  
-      $('#probar_letra').val('');
-      $('#adivinar').val('');
-  
-      $('#boton_iniciar').attr("disabled", false);
-  
-      $('#probar_letra').attr("disabled", true);
-      $('#boton_probar').attr("disabled", true);
-  
-      $('#adivinar').attr("disabled", true);
-      $('#boton_adivinar').attr("disabled", true);
-  
-      $('#boton_iniciar').focus();
-    },
-    
-    _iniciar : function(word) {
-
-      if (word.length > 0) {
-        if (App._cadenaPermitida(word)) {
-          word = word.toLowerCase();
-  
-          $('#boton_iniciar').attr("disabled", true);
-  
-          $('#probar_letra').attr("disabled", false);
-          $('#boton_probar').attr("disabled", false);
-  
-          $('#adivinar').attr("disabled", false);
-          $('#boton_adivinar').attr("disabled", false);
-  
-          App._establecerEspacios();
-  
-          $('#probar_letra').focus();
-        } else {
-          $('#etiqueta_mensaje').html('Datos Incorrectos');
-          $('#cuerpo_mensaje').html('La palabra debe contener caracteres de la A a la Z únicamente.');
-          $('#mensaje').modal('show')
-  
-          $('#mensaje').on('hidden.bs.modal', function () {
-            $('#boton_iniciar').focus();
-          })
-        }
-      } else {
-        $('#etiqueta_mensaje').html('Datos Incorrectos');
-        $('#cuerpo_mensaje').html('Debe escribir la palabra secreta.');
-        $('#mensaje').modal('show')
-  
-        $('#mensaje').on('hidden.bs.modal', function () {
-          $('#boton_iniciar').focus();
-        })
-      }
-    },
-
     minarNFT : function(word){
-      $.getJSON("Juego.json").success((data) => {
-
-        console.log(word);
-
-        const web3 = new Web3(App.web3Provider);
-        const acc = web3.eth.accounts;
-        const juego = TruffleContract(data);
-        juego.setProvider(App.web3Provider);
-        const instance = juego.at(App.accJuego);
         
+        App.instanciaJuego().then((res, err) => {
 
-        instance.mint(acc[0], word, {from: acc[0]}).then((res, err) => {
-          console.log(res);
-          console.log(res.logs[0].args.id.toNumber())
+          acc = res[0];
+          instance = res[1];
+
+          instance.mint(acc[0], word, {from: acc[0]}).then((res, err) => {
+            console.log(res);
+            console.log(res.logs[0].args.id.toNumber())
         })
-
-
       })
 
     }
@@ -489,15 +500,6 @@ $(document).ready(function () {
   };
 
   
-  
-
-
-  
-  //let jugador = App.crearJugador()
-  //console.log(App.elegirPalabra());
-
-  let fallos, aciertos, palabra_secreta, letras_probadas, letras_fallidas;
-
   function main() {
 
     let juego = App.initWeb3();
@@ -505,21 +507,13 @@ $(document).ready(function () {
   
     $('#boton_iniciar').click(App.elegirPalabra);
     $('#boton_probar').click(App._probarLetra);
-    $('#boton_finalizar').click(App._finalizar);
+    $('#boton_finalizar').click(Front._finalizar);
     $('#boton_adivinar').click(App._adivinar);
     $('#boton_crear').click(App.crearJugador);
-    $('#testNFT').click(App.minarNFT);
-
 
     $('#boton_crear').on("keydown", function (event) {
       if (event.which == 13) {
         App.crearJugador();
-      }
-    });
-
-    $('#testNFT').on("keydown", function (event) {
-      if (event.which == 13) {
-        App.minarNFT("luchador");
       }
     });
 
